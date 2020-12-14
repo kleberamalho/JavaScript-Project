0fa8849bd5aaaa2cbb29f51b626c3c2defe1b2b3
Kleber Ramalho <kleber.ramalho96@hotmail.com>
2020-12-14 16:04:10 -0300

    k

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+
+/*
+function Soma (n1, n2){
+    return n1 + n2;
+}
+
+function validaidade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade?");
+console.log(validaidade(idade));
+
+/*var d = new Date();
+alert(d.getMinutes());
+*/
+
+
+/* var count;
+
+for(count =1; count <=5; count++){
+   alert(count);
+}
+*/
+
+
+/*
+/*var count =0;
+while (count <= 5){
+console.log(count);
+alert(count);
+count++;
+}
+*/
+/*var idade = prompt("Qual sua idade");
+
+if (idade >= 18){  alert("Maior de idade");
+}else{  alert("menor de idade");
+}
+*/
+//var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva", cor: "roxa"}];
+//console.log(frutas);
+
+
+//var fruta = {nome:"maça", cor: "vermelha"};
+//console.log(fruta);
+
+//var lista = ["maça", "pêra", "laranja"];
+/*lista.push("uva");*/
+/*lista.pop();*/
+
+/*imprime o tamanho da lista console.log(lista.length); */
+
+/*console.log(lista.reverse()); */
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+/*
+var nome = "Kleber Ramalho";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * idade);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+*/
